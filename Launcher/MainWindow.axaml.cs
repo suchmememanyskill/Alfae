667f891bcbace1a3319b41ee927a15e481c6a5f1
suchmememanyskill <suchmememanyskill@users.noreply.github.com>
2022-07-05 01:58:03 +0200

    [App] allow ignoring new versions

--- Launcher/MainWindow.axaml.cs
+++ Launcher/MainWindow.axaml.cs
@@ -1,41 +1,73 @@
+using System.Collections.Generic;
+using System.IO;
 using Avalonia.Controls;
 using Avalonia.Threading;
 using Launcher.Utils;
 using Launcher.Views;
 using LauncherGamePlugin.Forms;
+using LauncherGamePlugin.Interfaces;
 
 namespace Launcher
 {
     public partial class MainWindow : Window
     {
+        private Loader.App _app;
+        
         public MainWindow()
         {
             InitializeComponent();
-            Loader.App app = Loader.App.GetInstance();
-            app.MainWindow = this;
+            _app = Loader.App.GetInstance();
+            _app.MainWindow = this;
             Dispatcher.UIThread.Post(Initialize);
             Content = new LoadingScreen();
         }
 
+        private async void CheckForUpdate()
+        {
+            string? gitVersion = await NewVersionCheck.GetGitVersion();
+            if (gitVersion != null && gitVersion != NewVersionCheck.Version)
+            {
+                string ignorePath = Path.Combine(_app.ConfigDir, "ignorever.txt");
+
+                string ignoreVersion = "";
+                if (File.Exists(ignorePath))
+                {
+                    ignoreVersion = await File.ReadAllTextAsync(ignorePath);
+                }
+
+                if (ignoreVersion != gitVersion)
+                {
+                    _app.ShowForm(new List<FormEntry>()
+                    {
+                        Form.TextBox($"Launcher has an update available, v{gitVersion}. Would you like to update?", FormAlignment.Center),
+                        Form.Button(
+                            "Back", x => _app.HideForm(),
+                            "Ignore this update", x =>
+                            {
+                                File.WriteAllText(ignorePath, gitVersion);
+                                _app.HideForm();
+                            },
+                            "Open webpage to new version", x => LauncherGamePlugin.Utils.OpenUrl("https://github.com/suchmememanyskill/Launcher/releases")
+                        )
+                    });
+                }
+            }
+        }
+        
         private async void Initialize()
         {
-            Loader.App app = Loader.App.GetInstance();
-            app.HeadlessMode = false;
-            await app.InitializeGameSources();
-            app.MainView = new MainView();
-            Content = app.MainView;
-            await app.ReloadGames2Task();
-            if (app.HasStartForm())
+            _app.HeadlessMode = false;
+            await _app.InitializeGameSources();
+            _app.MainView = new MainView();
+            Content = _app.MainView;
+            await _app.ReloadGames2Task();
+            if (_app.HasStartForm())
             {
-                app.ShowPossibleStartForm();
+                _app.ShowPossibleStartForm();
             }
             else
             {
-                string? gitVersion = await NewVersionCheck.GetGitVersion();
-                if (gitVersion != null && gitVersion != NewVersionCheck.Version)
-                {
-                    app.Show2ButtonTextPrompt($"Launcher has an update available, v{gitVersion}. Would you like to update?", "Back", "Update", x => app.HideForm(), x => LauncherGamePlugin.Utils.OpenUrl("https://github.com/suchmememanyskill/Launcher/releases"));
-                }
+               CheckForUpdate();
             }
         }
     }
