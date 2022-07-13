84c0c2d94d0561f8a8f223020f446ef43c94d87b
suchmememanyskill <suchmememanyskill@users.noreply.github.com>
2022-07-13 11:45:20 +0200

    [GOG] Fix offline mode

--- GogIntegration/Requests/GogApiAuth.cs
+++ GogIntegration/Requests/GogApiAuth.cs
@@ -21,12 +21,12 @@ public class GogApiAuth
     
     public static async Task<GogApiAuth?> Get(string code)
     {
-        using HttpClient client = new();
-        var response = await client.GetAsync(
-            $"https://auth.gog.com/token?client_id=46899977096215655&client_secret=9d85c43b1482497dbbce61f6e4aa173a433796eeae2ca8c5f6129f2dc4de46d9&grant_type=authorization_code&redirect_uri=https%3A%2F%2Fembed.gog.com%2Fon_login_success%3Forigin%3Dclient&code={code}");
-
         try
         {
+            using HttpClient client = new();
+            var response = await client.GetAsync(
+                $"https://auth.gog.com/token?client_id=46899977096215655&client_secret=9d85c43b1482497dbbce61f6e4aa173a433796eeae2ca8c5f6129f2dc4de46d9&grant_type=authorization_code&redirect_uri=https%3A%2F%2Fembed.gog.com%2Fon_login_success%3Forigin%3Dclient&code={code}");
+            
             response.EnsureSuccessStatusCode();
             string text = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<GogApiAuth>(text);
@@ -41,12 +41,12 @@ public class GogApiAuth
 
     public async Task<GogApiAuth?> Refresh()
     {
-        using HttpClient client = new();
-        var response = await client.GetAsync(
-            $"https://auth.gog.com/token?client_id=46899977096215655&client_secret=9d85c43b1482497dbbce61f6e4aa173a433796eeae2ca8c5f6129f2dc4de46d9&grant_type=refresh_token&refresh_token={RefreshToken}");
-
         try
         {
+            using HttpClient client = new();
+            var response = await client.GetAsync(
+                $"https://auth.gog.com/token?client_id=46899977096215655&client_secret=9d85c43b1482497dbbce61f6e4aa173a433796eeae2ca8c5f6129f2dc4de46d9&grant_type=refresh_token&refresh_token={RefreshToken}");
+            
             response.EnsureSuccessStatusCode();
             string text = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<GogApiAuth>(text);
