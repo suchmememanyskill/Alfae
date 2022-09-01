adecf929ef0e3be57dd321e65453115128aab9a4
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2022-09-01 13:23:32 +0200

    [Legendary] webview is unsupported on linux

--- LegendaryIntegration/Gui/LoginForm.cs
+++ LegendaryIntegration/Gui/LoginForm.cs
@@ -1,4 +1,5 @@
 ﻿using LauncherGamePlugin;
+using LauncherGamePlugin.Enums;
 using LauncherGamePlugin.Forms;
 
 namespace LegendaryIntegration.Gui;
@@ -15,15 +16,24 @@ public class LoginForm
     
     public void Show(string warningMessage = "")
     {
-        List<FormEntry> entries = new()
+        List<FormEntry> entries = new();
+        entries.Add(Form.TextBox("Log into Epic Games", alignment: FormAlignment.Center, fontWeight: "Bold"));
+
+        if (PlatformExtensions.CurrentPlatform == Platform.Windows)
         {
-            Form.TextBox("Log into Epic Games", alignment: FormAlignment.Center, fontWeight: "Bold"),
-            Form.Separator(),
-            Form.TextBox("Option 1: Login via embedded webpage", FormAlignment.Center),
-            Form.TextBox("Click the button below to launch a new window with the Epic Game Store's login page"),
-            Form.Button("Back", _ => _source.App.HideForm(), "Login using embedded page", _ => _source.Login()),
-            Form.Separator(),
-            Form.TextBox("Option 2: Login via authorizationCode", FormAlignment.Center),
+            entries.AddRange(new List<FormEntry>()
+            {
+                Form.Separator(),
+                Form.TextBox("Option 1: Login via embedded webpage", FormAlignment.Center),
+                Form.TextBox("Click the button below to launch a new window with the Epic Game Store's login page"),
+                Form.Button("Back", _ => _source.App.HideForm(), "Login using embedded page", _ => _source.Login()),
+                Form.Separator(),
+                Form.TextBox("Option 2: Login via authorizationCode", FormAlignment.Center),
+            });
+        }
+        
+        entries.AddRange(new List<FormEntry>()
+        {
             Form.TextBox("Click the link below, log in, and copy the authorizationCode value into the field below"),
             Form.ClickableLinkBox(EPICLOGINLINK, _ => Utils.OpenUrl(EPICLOGINLINK)),
             Form.TextInput("authorizationCode:"),
@@ -40,7 +50,7 @@ public class LoginForm
                     
                     _source.Login(authcode);
                 })
-        };
+        });
 
         if (warningMessage != "")
         {
