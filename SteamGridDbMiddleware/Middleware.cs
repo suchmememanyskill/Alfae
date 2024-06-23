2bd57f4696e6d3ff5a24c51355b5a0bf5b9c43cd
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2024-06-23 14:04:25 +0200

    SGDB: Only allow editing covers for installed games

--- SteamGridDbMiddleware/Middleware.cs
+++ SteamGridDbMiddleware/Middleware.cs
@@ -1,4 +1,5 @@
 ﻿using LauncherGamePlugin.Commands;
+using LauncherGamePlugin.Enums;
 using LauncherGamePlugin.Interfaces;
 using LauncherGamePlugin.Launcher;
 using SteamGridDbMiddleware.Gui;
@@ -24,14 +25,14 @@ public class Middleware : IServiceMiddleware
     public List<Command> GetGameCommands(IGame game, IGameSource next)
     {
         List<Command> commands = new(next.GetGameCommands(game));
+
+        if (_instance.Api == null || game.InstalledStatus != InstalledStatus.Installed) 
+            return commands;
         
-        if (_instance.Api != null)
-        {
-            commands.Add(new Command());
-            commands.AddRange(SteamGridDb.ImageTypes
-                    .Select(x => new Command($"Edit {x}", () => new OnImageEdit(game, _instance, x).ShowGui()))
-                    .ToList());
-        }
+        commands.Add(new Command());
+        commands.AddRange(SteamGridDb.ImageTypes
+            .Select(x => new Command($"Edit {x}", () => new OnImageEdit(game, _instance, x).ShowGui()))
+            .ToList());
 
         return commands;
     }
